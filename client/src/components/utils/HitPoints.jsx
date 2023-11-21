e193c6b08933e8364f8c48ef16778ccc5947dfd0
Duncan W. Saul <dunk.dws90@gmail.com>
2023-11-21 16:01:35 +0100

    first commit

--- client/src/components/utils/HitPoints.jsx
+++ client/src/components/utils/HitPoints.jsx
@@ -4,26 +4,57 @@ import MyButton from "./MyButton";
 
 function HitPoints() {
   const [count, setCount] = useState(0);
+  const [currentHp, setCurrentHp] = useState(30);
+  const [isUnconcious, setIsUnconcious] = useState(false);
+  const [isDead, setIsDead] = useState(false);
   const maxHp = 50;
 
-  const increment = () => {
-    setCount((prevCount) => prevCount + 1);
+  const handleChange = (e) => {
+    const parsed = parseInt(e.target.value);
+    setCount(parsed || 0);
   };
 
-  const decrement = () => {
-    if (count > 0) {
-      setCount((prevCount) => prevCount - 1);
+  const heal = () => {
+    if (currentHp < maxHp) {
+      const totalHp = currentHp + count;
+      if (totalHp > maxHp) {
+        setCurrentHp(maxHp);
+      } else {
+        setCurrentHp(totalHp);
+      }
+    }
+  };
+  const damage = () => {
+    if (currentHp > 0) {
+      const totalHp = currentHp - count;
+      console.log(totalHp);
+      if (totalHp < currentHp) {
+        setCurrentHp(0);
+      } else {
+        setCurrentHp(totalHp);
+      }
     }
   };
 
   return (
     <div className={styles.container}>
-      <h3 className={styles.label}>Hit Points</h3>
-      <p className={styles.hitPoints}>
-        {count} / {maxHp}
-      </p>
-      <MyButton handleClick={increment} label="Heal" />
-      <MyButton handleClick={decrement} label="Damage" />
+      <h3 className={styles.title}>Hit Points</h3>
+      <p className={styles.current}>Current</p>
+      <p className={styles.max}>Max</p>
+      <p className={styles.hitPoints}>{currentHp}</p>
+      <p className={styles.hitPoints}>/</p>
+      <p className={styles.hitPoints}>{maxHp}</p>
+      <div className={styles.containerBtn}>
+        <MyButton handleClick={heal} label="Heal" />
+        <input
+          className={styles.input}
+          id="hp"
+          type="number"
+          value={count}
+          onChange={handleChange}
+        />
+        <MyButton handleClick={damage} label="Damage" />
+      </div>
     </div>
   );
 }
